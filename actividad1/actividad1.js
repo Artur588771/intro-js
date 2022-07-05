bebe4556d3195ca8885b07dcd6602b00374d15ef
Arturo Renteria <arthur_metal88@hotmail.com>
2022-07-05 15:34:49 -0500

    Actividad1

---
+++ actividad1/actividad1.js
@@ -0,0 +1,21 @@
+/*Respuesta Primer Ejercicio*/
+let numero = prompt("Porfavor Ingresa un Numero");
+let resultado=numero*2;
+console.log(resultado);
+
+/*Respuesta Segundo Ejercicio*/
+let primerNumero = prompt("Porfavor Ingresa el primer Numero");
+let segundoNumero = prompt("Porfavor Ingresa el segundo Numero");
+console.log(primerNumero-segundoNumero);
+
+/*Respuesta Tercer Ejercicio*/
+let r=5.3;
+const PI = 3.141516;
+let area=PI*Math.pow(r,2);
+console.log(area);
+
+/*Respuesta Cuarto Ejercicio*/
+let D=8;
+let d=6;
+let a=D*d/2;
+console.log(a);
