c636f04de10e48eed2d824e0ab99ed0644fd21ba
Arturo Renteria <arthur_metal88@hotmail.com>
2022-07-09 21:20:35 -0500

    Vacuna Mejorada

--- vacuna/vacuna.js
+++ vacuna/vacuna.js
@@ -1,45 +1,46 @@
-/*Vacuna*/
-let edad=Number(prompt('Ingresa Tu edad Porfavor'));
+let fechaNac;
+let fechaActual=new Date();
+let yearDif;
+let mesDif;
+let diaDif;
+let zona;
 let sexo;
 let embarazada;
-let mesNac;
-let semGes;
-let zona;
-let permitir;
-if (edad>=18)
+let semGest;
+fechaNac=prompt('Teclea tu fecha de Nacimiento (DD/MM/AAAA)');
+yearDif=Number(fechaActual.getFullYear())-Number(fechaNac.substring(6,10));
+mesDif=Number(fechaNac.substring(3,5))-((Number(fechaActual.getMonth()))+1);
+diaDif=Number(fechaNac.substring(0,2))-(Number(fechaActual.getDate()));
+if((yearDif>18) || (yearDif==18 && mesDif<=0 && diaDif<=0))
 {
-   permitir=1; 
-   zona=prompt('¿Vives en Zona Fronteriza (S/N)');
-   mesNac=Number(prompt('¿Mes de Nacimiento (1 a 12)')); 
-   sexo=prompt('Ingresa tu Sexo (F/M)');
-   if(sexo=='F')
-   embarazada=prompt('¿Estas embarazada? (S/N)');   
-   if(embarazada=='S')
-   {
-   semGes=Number(prompt('Cuantos Semanas de gestacion tienes (en numero porfavor)'));
-   if(semGes>9)   
-   {
-   console.log('Felicidades Puedes vacunarte ya que tienes mas de 9 semanas de Gestion');          
-   }
-   else{
-   console.log('No puedes Vacunarte');         
-   permitir=0;
-   }   
-   }
-   if(zona=='S' && permitir==1)
-   {
-   console.log('Felicidades Puedes vacunarte ya que Vives en la zona fronteriza');       
-   }
-   else
-   {    
-    if((edad>=30 || (edad>=29 && mesNac>=7)) && permitir==1)
-    console.log('Felicidades Puedes vacunarte ya que cumples 30 años o mas este año');
-    else
-    console.log('No puedes Vacunarte');
-   }
+    zona=prompt('¿Resides en Zona fronteriza? (S/N)');
+
+    sexo=prompt('¿Cual es tu sexo? (F/M)');
+    if(sexo=='F')
+    {
+        embarazada=prompt('¿Estas embarazada?(S/N)');
+        if(embarazada=='S')
+        {
+        semGest=prompt('¿Tienes mas de 9 semanas de gestacion? S/N)');
+        if(semGest=='S')
+        {
+        console.log('Puedes Vacunarte ya que estas embarazada y 9+SG');
+        }
+        }
+    }
+    if((yearDif>=30 && sexo=='M') || (yearDif>=30 && sexo=='F' && embarazada=='N') || (yearDif>=30 && sexo=='F' && embarazada=='S' && semGest=='S') )
+    {
+        console.log('Puedes Vacunarte ya que cumples 30 años o mas este año');
+    }
+    else if((zona=='S' && sexo=='M') || (zona=='S' && sexo=='F' && embarazada=='N') || (zona=='S' && sexo=='F' && embarazada=='S' && semGest=='S') )
+    {
+        console.log('Puedes Vacunarte ya que eres mayor de 18 y resides en zona fronteriza');
+    }
+    else{
+        console.log('No puedes Vacunarte');
+    }
 
 }
-else
-{
-   console.log('No puedes Vacunarte');
+else{
+    console.log('No puedes Vacunarte porque eres menor de edad');
 }
