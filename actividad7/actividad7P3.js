57232fd9a96519bf098f053e9f8d8b28f1fe1936
Arturo Renteria <arthur_metal88@hotmail.com>
2022-07-12 22:51:53 -0500

    Actividad 7 Act

---
+++ actividad7/actividad7P3.js
@@ -0,0 +1,24 @@
+/* Creauna funcion recursiva para multiplicar dos numeros esperando dichos parametros*/
+function multiply(num1,num2){    
+    if(num2 === undefined || num2 === null || num2===''){
+        num2=prompt('Dame otro numero porfavor');
+        multiply(num1,num2);
+    }else
+    return console.log(num1*num2);        
+    }
+multiply(8,5);
+
+/*Crea una funcion para calcular la sucesion fibonacci*/
+function fibonacci(n) {
+    if (n <= 1) return n;
+
+    return fibonacci(n-1) + fibonacci(n-2);
+}
+
+for(let i=0;i<8;i++){
+    console.log(fibonacci(i));
+}
+
+
+
+
